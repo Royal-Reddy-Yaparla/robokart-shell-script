9e069995c4764ea2c5d46de32999341f43ccbccf
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-08 11:27:57 +0530

    mongodb install and congure

---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############################################
+# Author: ROYAL REDDY
+# Date: 08-04
+# Version: V1
+# Purpose: MongoDB install and configuration
+################################################
+
+# set up host
+
+
+ID=$(id -u)
+
+# Colors
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# Check root access to script
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R Error:: Provide root accuess to the script $N" 
+fi
---
+++ set_up_host.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+
+# Setup Hostname
+sudo hostnamectl set-hostname "$1"
+
+# Update the hostname part of Host File
+echo "`hostname -I | awk '{ print $1 }'` `hostname`" >> /etc/hosts
+
+# Refresh
+/bin/bash
